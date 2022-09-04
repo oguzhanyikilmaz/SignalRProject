0a0cc876b8dac7e5ed0c013c157bcac65a7baf9e
Oğuzhan <49417199+oguzhanyikilmaz@users.noreply.github.com>
2022-09-04 18:10:36 +0300

    Add project files.

---
+++ SignalRProject.Api/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SignalRProject.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        public HomeController()
+        {
+
+        }
+        [HttpGet()]
+        public IActionResult Get ()
+        {
+            return Ok();
+        }
+    }
+}
---
+++ SignalRProject.Api/ServiceHub/HubService.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using SignalRProject.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SignalRProject.Api.ServiceHub
+{
+    public class HubService:Hub
+    {
+        private static List<Users> UserList { get; set; } = new List<Users>();
+
+        public async Task SendMessage(string msg)
+        {
+            int id = (UserList.Count == 0 ? 0 : UserList.LastOrDefault().Id)+1;
+
+            UserList.Add(new Users { Id = id, Name = "Admin", Password = "1234" });
+
+            await Clients.All.SendAsync("UserList", UserList);
+        }
+    }
+}
